a3cc4edb06714a2515a093c74eee8ce672559dc1
Faye <96606732+Fayhee@users.noreply.github.com>
2022-07-14 03:01:19 +0100

    Update GetBalance

--- Assets/Scripts/Nervos/GetBalance.cs
+++ Assets/Scripts/Nervos/GetBalance.cs
@@ -16,36 +16,55 @@ public class GetBalance : MonoBehaviour
     public FirebaseUser User;
     public DatabaseReference DBreference;
 
-    public TMP_Text tokenText;
-
-    
-
-    public void ReadBalance()
+    public async void Start()
     {
+        auth = FirebaseAuth.DefaultInstance;
+        User = auth.CurrentUser;
 
-    //This class retrieves the user's account from firebase and gets the balance of the token
+        string account = User.DisplayName;
+        BigInteger balanceOf = -1;
+
+        if (rpc != null)
+        {
+            balanceOf = await ERC20.BalanceOf(chain, network, contract, account, rpc);
+        }
+        else
+        {
+            balanceOf = await ERC20.BalanceOf(chain, network, contract, account);
+        }
+
+        if (balanceOf == -1)
+        {
+            Debug.Log("Nothing");
+        }
+
+        if (balanceOf != null) balance1 = balanceOf.ToString();
+
+        string balance = balance1.Substring(0, 5);
+        tokenField.text = balance;
+        StartCoroutine(UpdateBalance(int.Parse(balance)));
+    }
+    
+    private IEnumerator UpdateBalance(int balance)
+    {
         auth = FirebaseAuth.DefaultInstance;
         DBreference = FirebaseDatabase.DefaultInstance.RootReference;
         User = auth.CurrentUser;
-        string balance = "";
-        //Get the currently logged in user data
-        DBreference.Child("users").Child(User.UserId).GetValueAsync().ContinueWith(task =>
+        //Set the currently logged in user deaths
+        var DBTask = DBreference.Child("users").Child(User.UserId).Child("balance").SetValueAsync(balance);
+
+        yield return new WaitUntil(predicate: () => DBTask.IsCompleted);
+
+        if (DBTask.Exception != null)
         {
-            if (task.IsCompleted)
-            {
-                DataSnapshot snapshot = task.Result;
-                balance = snapshot.Child("balance").Value.ToString();
-            
-
-                tokenText.text = balance;
-
-            }
-            else
-            {
-                tokenText.text = "0";
-            }
-        });
-
-
+            Debug.LogWarning(message: $"Failed to register task with {DBTask.Exception}");
+        }
+        else
+        {
+            Debug.Log("balance are now updated");
+        }
     }
+
+
+   
 }
